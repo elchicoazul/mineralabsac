e60aea0272635cf5b34c62ed26c48cfb73a5e0ea
Kenneth Motta <kenneth.motta@vfcons.com>
2022-10-27 22:23:39 -0500

    notification

--- app/Views/Tipocliente/index.php
+++ app/Views/Tipocliente/index.php
@@ -59,5 +59,22 @@
                 </div>
             </div>
 </div>
-        
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script type="text/javascript">
+let mensaje = '<?php echo $mensaje ?>';
+
+if (mensaje == '1') {
+    swal(':D', 'Creado con éxito', 'success');
+} else if (mensaje == '0') {
+    swal(':(', 'Fallo al agregar!', 'error');
+} else if (mensaje == '2') {
+    swal(':D', 'Actualizado con exito', 'success');
+} else if (mensaje == '3') {
+    swal(':(', 'Fallo al Actualizar!', 'error');
+} else if (mensaje == '4') {
+    swal(':D', 'Eliminado con exito!', 'success');
+} else if (mensaje == '5') {
+    swal(':(', 'Fallo al eliminar!', 'error');
+}
+</script>         
 <?= $this->endSection()?>
